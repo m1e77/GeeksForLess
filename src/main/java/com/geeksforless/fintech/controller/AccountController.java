e79e98a34858414606e27ed48f771f153cbf096d
Andrii Pylypenko <andrii.pylypenko@coro.net>
2023-10-22 15:16:44 +0300

    Add java docs to controller

--- src/main/java/com/geeksforless/fintech/controller/AccountController.java
+++ src/main/java/com/geeksforless/fintech/controller/AccountController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * A RESTful controller responsible for handling account-related operations.
+ */
 @Validated
 @RestController
 @RequestMapping("/api/accounts")
@@ -21,12 +24,24 @@ public class AccountController {
     private final AccountService accountService;
     private final AccountMapper accountMapper;
 
+    /**
+     * Transfers money between accounts.
+     *
+     * @param transferDto the data transfer object containing transfer details
+     * @return a ResponseEntity indicating the result of the operation
+     */
     @PostMapping("/transfer")
     public ResponseEntity<Void> transferMoney(@RequestBody @Valid MoneyTransferDto transferDto) {
         accountService.transfer(transferDto.getFromAccountId(), transferDto.getToAccountId(), transferDto.getAmount());
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Retrieves the details of a specific account by its ID.
+     *
+     * @param id the ID of the account to retrieve
+     * @return a ResponseEntity containing the account details or a not-found status
+     */
     @GetMapping("/{id}")
     public ResponseEntity<AccountDetailsDto> getAccount(@PathVariable Long id) {
         return accountService.get(id)
@@ -35,6 +50,11 @@ public class AccountController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Retrieves the details of all available accounts.
+     *
+     * @return a ResponseEntity containing a list of account details
+     */
     @GetMapping
     public ResponseEntity<List<AccountDetailsDto>> getAccounts() {
         return ResponseEntity.ok(
